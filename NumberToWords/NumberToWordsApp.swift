4dca4811a7952f4a6f9a2dd4c013496b479575d2
Rajakani <rjkani@gmail.com>
2023-06-22 20:34:44 +0530

    Initial Commit

---
+++ NumberToWords/NumberToWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberToWordsApp.swift
+//  NumberToWords
+//
+//  Created by Priyalakshmi Rajakani on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NumberToWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
